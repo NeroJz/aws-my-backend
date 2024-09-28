2f80ff84d532dea4395a023d3db959d79b95dd70
KumHoe <nerojz86@gmail.com>
2024-09-28 11:40:17 +0800

    Add log in endpoint

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,14 @@ app.use(json());
 app.use(cors());
 
 app.get('/health', (req: Request, res: Response) => {
+  console.log(`Receive request in /health endpoint`);
   res.status(200).send({
     msg: 'App is healthy'
   });
 });
 
 app.get('/info', (req: Request, res: Response) => {
+  console.log(`Receive request in /info endpoint`);
   res.send({
     data: 'Info return from backend'
   });
